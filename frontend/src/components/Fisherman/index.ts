dadef9d19fd71e86c7f113a3657e3a2cd0591e9f
Abhinav <nakkaabhinav19@gmail.com>
2026-09-10 14:01:38 +0530

    feat(frontend): export Fisherman components

---
+++ frontend/src/components/Fisherman/index.ts
@@ -0,0 +1,5 @@
+export { FishermanPanel } from "./FishermanPanel";
+export { FishermanVerdict } from "./FishermanVerdict";
+export type { VerdictValue } from "./FishermanVerdict";
+export { VoiceInput } from "./VoiceInput";
+export { VoiceOutput } from "./VoiceOutput";
